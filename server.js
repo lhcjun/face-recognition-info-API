a5f559ee2cd5d3291669349385a88b4366dc065b
Jun <lhcjun@gmail.com>
2020-03-28 20:40:21 +0800

    fix cors

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(cors());
 // CORS
 app.use((req, res, next) => {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
