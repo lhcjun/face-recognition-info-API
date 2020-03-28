1393dce057b41fa76d0e84b6018d65ef3678bb55
Jun <lhcjun@gmail.com>
2020-03-28 20:16:12 +0800

    fix cors

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(cors());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, application/json, Content-Type, Accept");
     next();
 });
 // Data URI : PayloadTooLargeError
